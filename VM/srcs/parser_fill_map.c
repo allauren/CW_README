e8360890a693895cda76d5076650fca196e3e14a
Alexis LAURENT <allauren@e1r8p18.42.fr>
2018-02-13 16:49:52 +0100

    debut du parsing

---
+++ VM/srcs/parser_fill_map.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_fill_map.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: allauren <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/02/13 14:20:12 by allauren          #+#    #+#             */
+/*   Updated: 2018/02/13 14:58:01 by allauren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "display.h"
+
+void	ft_fill_map(unsigned char *map, unsigned char *buf, int start, int stop)
+{
+	int		i;
+	int		z;
+
+	i = start;
+	z = -1;
+	while (++z < stop)
+		map[i++] = buf[z];
+}

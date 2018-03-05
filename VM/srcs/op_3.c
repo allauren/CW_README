fea6a9cda353772d8e93902b8e24c663493dfc0d
Guilhem SMITH <gsmith@student.42.fr>
2018-03-05 13:14:22 +0100

    implementation des forks

--- VM/srcs/op_3.c
+++ VM/srcs/op_3.c
@@ -6,7 +6,7 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/14 13:15:44 by gsmith            #+#    #+#             */
-/*   Updated: 2018/02/28 16:17:55 by gsmith           ###   ########.fr       */
+/*   Updated: 2018/03/05 13:11:53 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,5 +55,10 @@ void	op_sti(t_memory *mem, t_proc *proc, t_proc **lst_proc, t_timer *timer)
 
 void	op_fork(t_memory *mem, t_proc *proc, t_proc **lst_proc, t_timer *timer)
 {
-	return ;
+	unsigned int		dest;
+
+	(void)timer;
+	dest = proc->pc + (read_mem(mem, (proc->pc) + 1, 2) % IDX_MOD);
+	proc_add(lst_proc, proc_dup(proc, dest));
+	(proc->pc) += 3;
 }
--- VM/srcs/op_4.c
+++ VM/srcs/op_4.c
@@ -6,7 +6,7 @@
 /*   By: gsmith <gsmith@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/14 13:15:44 by gsmith            #+#    #+#             */
-/*   Updated: 2018/02/28 16:20:11 by gsmith           ###   ########.fr       */
+/*   Updated: 2018/03/05 13:12:27 by gsmith           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	op_lld(t_memory *mem, t_proc *proc, t_proc **lst_proc, t_timer *timer)
 
 	(void)lst_proc;
 	(void)timer;
-	opc = read_mem(mem, proc->pc + 1, 1);
+	ocp = read_mem(mem, proc->pc + 1, 1);
 	if (!ARG(ocp, 1) && ARG(ocp, 2) == A_REG
 			&& (ARG(ocp, 3) == A_DIR || ARG(ocp, 3) == A_IND))
 	{
@@ -48,7 +48,12 @@ void	op_lldi(t_memory *mem, t_proc *proc, t_proc **lst_proc, t_timer *timer)
 
 void	op_lfork(t_memory *mem, t_proc *proc, t_proc **lst_proc, t_timer *timer)
 {
-	return ;
+	unsigned int		dest;
+
+	(void)timer;
+	dest = proc->pc + read_mem(mem, (proc->pc) + 1, 2);
+	proc_add(lst_proc, proc_dup(proc, dest));
+	(proc->pc) += 3;
 }
 
 void	op_aff(t_memory *mem, t_proc *proc, t_proc **lst_proc, t_timer *timer)
@@ -56,7 +61,7 @@ void	op_aff(t_memory *mem, t_proc *proc, t_proc **lst_proc, t_timer *timer)
 	unsigned int	ocp;
 	char			rg;
 
-	(void)lst_process;
+	(void)lst_proc;
 	(void)timer;
 	ocp = read_mem(mem, proc->pc + 1, 1);
 	if (ARG(ocp, 1) == A_REG && !ARG(ocp, 2) && !ARG(ocp, 3))
